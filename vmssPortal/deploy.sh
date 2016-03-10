42f6f358ff3040ef0bfb78cb55791caf26e705c3
Neil Sant Gat <negat@microsoft.com>
2016-03-10 20:26:40 +0000

    initial commit of vmssPortal template

---
+++ vmssPortal/deploy.sh
@@ -0,0 +1,24 @@
+echo "{" > azuredeploy.parameters.json
+echo "  \"$schema\": \"http://schema.management.azure.com/schemas/2015-01-01/deploymentParameters.json#\"," >> azuredeploy.parameters.json
+echo "  \"contentVersion\": \"1.0.0.0\"," >> azuredeploy.parameters.json
+echo "  \"parameters\": {" >> azuredeploy.parameters.json
+echo "    \"image\": {" >> azuredeploy.parameters.json
+echo "      \"value\": \"Ubuntu\"" >> azuredeploy.parameters.json
+echo "    }," >> azuredeploy.parameters.json
+echo "    \"vmssName\": {" >> azuredeploy.parameters.json
+echo "      \"value\": \"$1\"" >> azuredeploy.parameters.json
+echo "    }," >> azuredeploy.parameters.json
+echo "    \"instanceCount\": {" >> azuredeploy.parameters.json
+echo "      \"value\": 2" >> azuredeploy.parameters.json
+echo "    }," >> azuredeploy.parameters.json
+echo "    \"adminUsername\": {" >> azuredeploy.parameters.json
+echo "      \"value\": \"negat\"" >> azuredeploy.parameters.json
+echo "    }," >> azuredeploy.parameters.json
+echo "    \"adminPassword\": {" >> azuredeploy.parameters.json
+echo "      \"value\": \"P4ssw0rd\"" >> azuredeploy.parameters.json
+echo "    }" >> azuredeploy.parameters.json
+echo "  }" >> azuredeploy.parameters.json
+echo "}" >> azuredeploy.parameters.json
+
+
+azure group create -n $1rg -d $1dep -l "West US" -f azuredeploy.json -e azuredeploy.parameters.json
