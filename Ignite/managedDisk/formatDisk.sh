1285fbe1c54a3d8dcc4f6ec2eee74b7a91f609b6
negat <negat@microsoft.com>
2016-09-23 20:07:27 +0000

    added comments

--- Ignite/managedDisk/formatDisk.sh
+++ Ignite/managedDisk/formatDisk.sh
@@ -1,5 +1,9 @@
+# note: this script doesn't do anything with fstab, which it probably should
+# also, it doesn't fix any directory permissions if run as root, which it probably should
+
 parted /dev/sdc mklabel gpt
 parted /dev/sdc mkpart primary 0% 100%
 sudo mkfs.ext4 /dev/sdc1
 mkdir /home/negat/data
 mount /dev/sdc1 /home/negat/data
+
