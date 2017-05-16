cf8842c702e3fd7009e90435744b19e2eea7d8e3
negat <negat@microsoft.com>
2017-05-16 00:45:07 +0000

    made app actually get events

--- topologyNotifications/get_topology_notifications.py
+++ topologyNotifications/get_topology_notifications.py
@@ -1,10 +1,31 @@
 import azurerm
 import sys
+import datetime
+
+
+# P0 - get an access token from a user-generated service principal (shown below)
+# P1 - instead of the below, get an access token from MSI (should just be a call to an internal IP)
+
+tenantId = sys.argv[1]
+applicationId = sys.argv[2]
+applicationSecret = sys.argv[3]
+scaleSetResourceId = sys.argv[4]
+subscriptionId = scaleSetResourceId.split('/')[1]
 
 access_token = azurerm.get_access_token(
-    sys.argv[1],
-    sys.argv[2],
-    sys.argv[3]
+    tenantId,
+    applicationId,
+    applicationSecret
 )
 
-print(access_token)
+
+startTimestamp = str(datetime.datetime.now() - datetime.timedelta(days=1))
+
+endpoint = ''.join([azure_rm_endpoint,
+                    '/subscriptions/', subscriptionId,
+                    '/providers/microsoft.insights/eventtypes/management/values?api-version=2015-04-01&$filter=eventTimestamp ge \'',
+                    startTimestamp, '\''])
+res = azurerm.do_get(endpoint, access_token)
+
+print(res)
+print(res.text)
